e19172b731531595e56887977736a6daf3745f95
vlad <vnelipovich63@gmail.com>
2023-02-20 20:05:05 +0300

    used callback, onClick, onChange, onBlur

---
+++ vanilla/src/05/06-callbacks.tsx
@@ -0,0 +1,21 @@
+import {ChangeEvent, MouseEvent} from 'react'
+export const User = () => {
+
+    const deleteUser = (event: MouseEvent<HTMLButtonElement>) => {
+        alert(event.currentTarget.name)
+    }
+
+    const onNameChanged = () => {
+        console.log('name changed')
+    }
+    const focusLostHandler = () => {
+        console.log('focus lost')
+    }
+    const onAgeChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log('age changed '+ event.currentTarget.value)
+    }
+    return <div><textarea onChange={onNameChanged} onBlur={focusLostHandler}>Dimych</textarea>
+        <input onChange={onAgeChanged} type={"number"}/>
+        <button name="delete" onClick={deleteUser}>delete</button>
+    </div>
+}
