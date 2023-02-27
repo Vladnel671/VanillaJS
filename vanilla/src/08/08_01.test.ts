4183ddc0b41191372d10762b86f099ac2bf1e891
vlad <vnelipovich63@gmail.com>
2023-02-27 21:31:35 +0300

    Associative array

---
+++ vanilla/src/08/08_01.test.ts
@@ -0,0 +1,32 @@
+type UserType = {
+    [key: string]: {id:number, name: string}
+}
+
+let users : UserType
+
+beforeEach( () => {
+    users = {
+        '101': {id: 101, name: 'Vlad'},
+        '3232312': {id: 3232312, name: 'Natasha'},
+        '1212': {id: 1212, name: 'Valera'},
+        '1': {id: 1, name: 'Katya'}
+    }
+
+})
+
+test("should update corresponding user from obj", ()=> {
+
+    users["1"].name= 'Ekaterina'
+
+
+    expect(users['1']).toEqual({id: 1, name: 'Ekaterina'})
+
+})
+test("should delete corresponding user from obj", ()=> {
+
+    delete users["1"]
+
+
+    expect(users['1']).toBeUndefined()
+
+})
---
+++ vanilla/src/08/08_01.ts
@@ -0,0 +1,16 @@
+export const userObj = {
+    '0': 'Vlad',
+    '1': 'Natasha',
+    '2': 'Valera',
+    '3': 'Katya'
+}
+
+
+
+
+export const usersArray = [
+    {id: 101, name: 'Vlad'},
+    {id: 3232312, name: 'Natasha'},
+    {id: 1212, name: 'Valera'},
+    {id: 1, name: 'Katya'}
+]
